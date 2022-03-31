b2d49236430d13f9a8303284b83c22a56d4a8ea6
CaioJordan <caiojbm@taginfraestrutura.com.br>
2022-03-31 17:31:14 -0300

    RemoveCache

---
+++ RentGames.Core/StorageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentGames.Core
+{
+    public class StorageService
+    {
+        public StorageService()
+        {
+
+        }
+    }
+}
---
+++ RentGamesApi/Controllers/StorageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentGamesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StorageController : ControllerBase
+    {
+
+        public StorageController()
+        {
+
+        }
+       
+
+        [HttpGet]
+        public int GetAll([FromRoute] bool? all)
+        {
+            //TokenAuth0
+
+            //CallTheService
+
+            return 1;
+        }
+
+    }
+}
